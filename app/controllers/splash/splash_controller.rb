977f08bc5c4db43258a1a7aedce8c6b482cfb2cc
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-30 21:47:59 +0000

    add controller for splash page

---
+++ app/controllers/splash/splash_controller.rb
@@ -0,0 +1,10 @@
+<div class="">
+  <div class = "">
+    <h1>Budget Application</h1>
+  </div>
+
+  <div class="">
+    <%= link_to "Login", new_user_session_path, class: "" %>
+    <%= link_to "Signup", new_user_registration_path, class: "" %>
+  </div>
+</div>
