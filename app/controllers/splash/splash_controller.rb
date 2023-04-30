be66540cc156876408cafe8e668112da429fce9b
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-30 21:52:24 +0000

    update the splash controller

--- app/controllers/splash/splash_controller.rb
+++ app/controllers/splash/splash_controller.rb
@@ -1,10 +1,5 @@
-<div class="">
-  <div class = "">
-    <h1>Budget Application</h1>
-  </div>
-
-  <div class="">
-    <%= link_to "Login", new_user_session_path, class: "" %>
-    <%= link_to "Signup", new_user_registration_path, class: "" %>
-  </div>
-</div>
+class SplashController < ApplicationController
+  def index
+    render 'splash'
+  end
+end
