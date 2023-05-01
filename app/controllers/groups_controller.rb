99cc98ce7202740b5c9225409bbfa4ef9e0bc9cf
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-01 10:22:14 +0000

    create groups controller

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,31 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @groups = Group.order(created_at: :desc).all
+    @user = current_user
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.author_id = current_user.id
+    if @group.save
+      redirect_to authenticated_root_path, notice: 'Group was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
+end
