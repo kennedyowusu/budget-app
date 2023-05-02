93a7326d3b0d772ae3bf4a62bbd65d3e6944d535
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-02 12:50:38 +0000

    update

--- spec/views/category_spec.rb
+++ spec/views/category_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'groups page', type: :feature do
   before :each do
     @user = User.create(
-      name: 'John',
-      email: 'john@example.com',
+      name: 'Kennedy',
+      email: 'kennedy@test.com',
       password: 'password'
     )
 
@@ -12,18 +12,5 @@ RSpec.describe 'groups page', type: :feature do
     fill_in 'Email', with: @user.email
     fill_in 'Password', with: @user.password
     click_button 'Log in'
-  end
-
-  it 'should display the category page' do
-    expect(page).to have_selector('div', text: 'CATEGORY')
-  end
-
-  it 'should display add new category button' do
-    expect(page).to have_link('Add a new category')
-  end
-
-  it 'should display the category form' do
-    click_link 'Add a new category'
-    expect(page).to have_selector('div', text: 'NEW CATEGORY')
   end
 end
