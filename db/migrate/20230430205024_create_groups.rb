b99d7758396871caa1492558ccb5178dea9782d7
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-02 15:31:26 +0000

    make the name fields none null

--- db/migrate/20230430205024_create_groups.rb
+++ db/migrate/20230430205024_create_groups.rb
@@ -1,8 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
-      t.string :name
-      t.string :icon
+      t.string :name, null: false
+      t.string :icon, null: false
       t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
--- db/migrate/20230502113826_create_expenses.rb
+++ db/migrate/20230502113826_create_expenses.rb
@@ -1,8 +1,8 @@
 class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
-      t.string :name
-      t.decimal :amount
+      t.string :name, null: false
+      t.decimal :amount, null: false
       t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
