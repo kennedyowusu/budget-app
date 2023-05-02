7119b6870fb7c9b2778d9d9e6f223209f23e7cd6
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-02 13:13:42 +0000

    fix issue with the naming convention for the join table

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,6 @@
 class Expense < ApplicationRecord
-  has_and_belongs_to_many :groups
+  # has_and_belongs_to_many :groups
+  has_and_belongs_to_many :groups, join_table: :join_group_and_expenses
   belongs_to :author, class_name: 'User'
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
